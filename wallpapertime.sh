637ebd844e36315db4ce86de9cf3c5de1121b662
GlitchedULemur <jackb49@ymail.com>
2017-05-19 04:51:26 -0500

    Add quick start commands for Cogmind, DarkestDungeon, SunlessSea, and Trackmania

--- wallpapertime.sh
+++ wallpapertime.sh
@@ -2,14 +2,14 @@
 
 #fill an array with the pictures
 declare -A bgs
-bgs[0]="$HOME/Pictures/8bitday/0.png"
-bgs[1]="$HOME/Pictures/8bitday/1.png"
-bgs[2]="$HOME/Pictures/8bitday/2.png"
-bgs[3]="$HOME/Pictures/8bitday/3.png"
-bgs[4]="$HOME/Pictures/8bitday/4.png"
-bgs[5]="$HOME/Pictures/8bitday/5.png"
-bgs[6]="$HOME/Pictures/8bitday/6.png"
-bgs[7]="$HOME/Pictures/8bitday/7.png"
+bgs[2]="$HOME/Pictures/8bitday/0.png"
+bgs[3]="$HOME/Pictures/8bitday/1.png"
+bgs[4]="$HOME/Pictures/8bitday/2.png"
+bgs[5]="$HOME/Pictures/8bitday/3.png"
+bgs[6]="$HOME/Pictures/8bitday/4.png"
+bgs[7]="$HOME/Pictures/8bitday/5.png"
+bgs[0]="$HOME/Pictures/8bitday/6.png"
+bgs[1]="$HOME/Pictures/8bitday/7.png"
 
 while true; do
     # set the current hour, drop leading zeros, split day into 8 parts
